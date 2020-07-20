82034ba2940c22303a4f08ce106e1242da2b6e6b
Conrad Buys <conradbuys11@gmail.com>
2020-07-20 23:03:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,17 @@
 # code your #position_taken? method here!
 
-board_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#board_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#
+#def position_taken?(board_array, space_chosen_int)
+#  if board_array[space_chosen_int - 1] == " "      #this space is empty
+#    board_array[space_chosen_int - 1] = "X"
+#    #allow move
+#  else
+#    puts "Please select another place! Or Whatever!"
+#    #do something else
+#  end
+#end
 
-def position_taken?(board_array, space_chosen_int)
-  if board_array[space_chosen_int - 1] == " "      #this space is empty
-    board_array[space_chosen_int - 1] = "X"
-    #allow move
-  else
-    puts "Please select another place! Or Whatever!"
-    #do something else
-  end
+def #position_taken?()
+
 end
