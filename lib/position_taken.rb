a24a234893f9384cb2b70dd0579e663ef852b8d7
Conrad Buys <conradbuys11@gmail.com>
2020-07-20 22:59:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 board_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board_array, space_chosen_int)
-  if  board_array[]       #this space is empty
+  if board_array[space_chosen_int - 1] ==       #this space is empty
     #allow move
   else
     #do something else
